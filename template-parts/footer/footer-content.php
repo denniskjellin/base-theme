b2275db538ba38e8413aa63d18268ba4e0e61fe1
Dennis Kjellin <dennis.kjellin@knowit.se>
2024-09-20 13:49:22 +0200

    template parts

---
+++ template-parts/footer/footer-content.php
@@ -0,0 +1,23 @@
+
+<footer class="footer mt-5">
+    <div class="footer__content">
+        <div class="footer__links">
+            <a href="#">Hem</a>
+            <a href="#">Om oss</a>
+            <a href="#">Kontakt</a>
+            <a href="#">Tjänster</a>
+            <a href="#">Nyheter</a>
+        </div>
+    <div class="footer__copyright-test">
+        <p><?php echo get_bloginfo('name') ;?> all rights reserved</p>
+    </div>
+        <div class="footer__social-icons">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.svg" alt="Besök oss på Facebook." aria-label="Besök oss på Facebook.">
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.svg" alt="Besök oss på Instagram." aria-label="Besök oss på Instagram.">
+            </a>
+        </div>
+    </div>
+</footer>
